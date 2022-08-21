a88945483ef640061ce193d545a8a93a66c6a6a0
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-08-21 00:52:18 -0300

    Model declaration

---
+++ app/src/main/java/br/com/zup/tana/data/model/Dish.kt
@@ -0,0 +1,27 @@
+package br.com.zup.tana.data.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = "dish")
+data class Dish(
+
+    @PrimaryKey(autoGenerate = false)
+    @SerializedName("name")
+    val name:String,
+
+    @SerializedName("description")
+    val description:String,
+
+    @SerializedName("value")
+    val value:Double,
+
+    @SerializedName("urlImageProduct")
+    val urlImageProduct:String,
+
+    var isFavorite: Boolean = false
+) : Parcelable
